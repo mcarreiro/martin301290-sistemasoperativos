8d2a9ddedf1cfe17086192c487257c96fecf121a
kevinkuja@gmail.com <kevinkuja@gmail.com@bc70af33-96d5-9d1e-8836-0a54cdf17148>
2012-04-14 15:14:17 +0000

    Round Robin

--- tp1/simusched/sched_rr.cpp
+++ tp1/simusched/sched_rr.cpp
@@ -10,7 +10,6 @@ using namespace std;
 SchedRR::SchedRR(vector<int> argn) {
 	quantum = argn[0]; //Recibe quantum
 	contador = 0;
-	bloqueada = -2;
 }
 
 void SchedRR::load(int pid) {
@@ -18,55 +17,122 @@ void SchedRR::load(int pid) {
 }
 
 void SchedRR::unblock(int pid) {	
-	bloqueada = -2; //No hay bloqueada
+	vector<int>::iterator it;
+  
+	for ( it= bloqueados.begin() ; it < bloqueados.end(); it++ ){
+		if(*it == pid){
+			bloqueados.erase(it);
+			break;
+		}
+	}
+    
 }
 
 int SchedRR::tick(const enum Motivo m) {  	
-	if(bloqueada != -2){			
-			unblock(bloqueada);		
-			
-	}	
+	int tamCola;
 	switch(m){
 	case EXIT:
 		// Si el pid actual terminó, sigue el próximo.
-		if (q.empty()) return IDLE_TASK;
-		else {
-			int sig = q.front(); q.pop();			
+		if (q.empty()) {return IDLE_TASK;}
+		else {		
 			contador = 0;			
-			return sig;
+			tamCola = q.size();
+			for(int i = 0; i < tamCola;i++)
+				{
+						int sig = q.front(); q.pop();
+						if(NoEstaBloqueado(sig))
+							{								
+								return sig;
+							}
+						else{
+							q.push(sig);
+						}
+							
+				}
+				return IDLE_TASK;
 		}
 	break;
 	case BLOCK:		
-			bloqueada = current_pid();
-			return next();
+			bloqueados.insert(bloqueados.begin(), current_pid());
+			contador = 0;
+			tamCola = q.size();
+				for(int i = 0; i < tamCola;i++)
+				{
+						int sig = q.front(); q.pop();
+						if(NoEstaBloqueado(sig))
+							{
+								q.push(current_pid());
+								return sig;
+							}
+						else{
+							q.push(sig);
+						}
+							
+				}
+				return IDLE_TASK;
 			
 	break;
 	case TICK:
-			contador += 1;
-			if(contador == quantum)
-			{	//Se me acabo el quantum, tengo que dejar al siguiente
-				return next();
+			// Tenemos que ver si se estaba ejecutando la IDLE o no...
+			if (current_pid() == IDLE_TASK)	// Caso que se estaba ejecutando la IDLE
+			{
+				//Si no tengo procesos devuelvo la idle ...
+				if (q.empty())	{return IDLE_TASK;}
 				
-			}else{
-				return current_pid(); //Sigo YO
-			}	
+				//Si tengo procesos devuelvo alguno que no este bloqueado ...
+				tamCola = q.size();
+				for(int i = 0; i < tamCola;i++)
+				{
+						int sig = q.front(); q.pop();
+						if(NoEstaBloqueado(sig)){
+							return sig;
+						}
+						else{
+							q.push(sig);
+						}
+							
+				}
+				
+			}
+			else 
+			{
+				contador ++;				// Aumentamos el quantum consumido del proceso actual
+				if(contador == quantum)		// Si alcanzo el quantum maximo...
+				{	
+					contador = 0;			// Reseteo el quantum				
+				
+					tamCola = q.size();
+					for(int i = 0; i < tamCola;i++)
+					{
+							int sig = q.front(); q.pop();
+							if(NoEstaBloqueado(sig))
+							{
+								q.push(current_pid());
+								return sig;
+							}
+							else{
+								q.push(sig);	
+							}						
+					}
+
+				}
+				return current_pid();	
+			}		// Si no se alcanzo el quantum se sigue ejecutando el current
 	
 	break;
 	}
 }
 
-int SchedRR::next(){
-		if (q.empty()) {			
-			return current_pid();
-		}
-		else {
-			int sig = q.front(); q.pop();
-			q.push(current_pid());
-			contador = 0;			
-			return sig;
-		}	
-		
-}		
+
+bool SchedRR::NoEstaBloqueado(int pid)
+{
+	for(int i=0;i < bloqueados.size();i++){
+			if(bloqueados[i] == pid){
+				return true;
+			}
+	}
+	return false;
+}
 	
 	
 	
