df4bda4e8d7227de8a58ffcfb53111bad306cdad
martin301290@gmail.com <martin301290@gmail.com@bc70af33-96d5-9d1e-8836-0a54cdf17148>
2012-04-14 18:33:53 +0000

    loteria andandno

--- tp1/simusched/sched_lottery.cpp
+++ tp1/simusched/sched_lottery.cpp
@@ -6,13 +6,112 @@
 using namespace std;
 
 SchedLottery::SchedLottery(vector<int> argn) {
+	srand((unsigned int) argn[0]);
+	quantum = (unsigned int) argn[1];
+	contador = 0;
+	ticketsTotal = 0;	
 }
 
 void SchedLottery::load(int pid) {
+	tarea nuevaTarea(pid,1,NORMAL);
+	q.push_back(nuevaTarea);					// Lo agrego a la cola de procesos
+	ticketsTotal++;	
 }
 
 void SchedLottery::unblock(int pid) {
+	int i = buscarProcesoEnCola(pid);
+	q[i].estado = SOBRAN_TICKETS;
+	ticketsTotal += q[i].tickets;	// Recupero los tickets del proceso bloqueado para que entre en el sorteo
+
 }
 
 int SchedLottery::tick(const enum Motivo m) {
+	switch(m)
+	{
+		case BLOCK: return haceBlock();
+		case TICK:  return haceTick();
+		case EXIT:  return haceExit();
+	}		
+}
+
+int SchedLottery::haceBlock(){
+	contador ++;	//Esto es porque al menos uso uno del CPU y para darle la compensacion no puede ser cero
+	int i = buscarProcesoEnCola(current_pid());
+	ticketsTotal -= q[i].tickets;
+	q[i].tickets += quantum / contador; // Le asigno la compensacion definida como 1/(contador/quantum) = quantum/contador
+	q[i].estado = BLOQUEADO; // esta bloqueado y tiene tickets de mas, este es el estado de bloqueado
+
+	if (ticketsTotal > 0) { //Quedan tareas en la cola de activas
+		int ticketGanador = random() % ticketsTotal;			// Consigo el ticket ganador
+		contador = 0; 							// Reseteo contador para el proximo
+		int indiceGanador = sorteo(ticketGanador);
+		sacarTicketsDeMas(indiceGanador);
+		return q[indiceGanador].pid;
+	}else{
+		return IDLE_TASK;	
+	}
+}
+
+int SchedLottery::haceTick(){
+	contador ++;
+	if ((quantum == contador || current_pid() == IDLE_TASK) && ticketsTotal > 0) { //Debo cambiar de tarea y hay más para hacerlo
+		int ticketGanador = random() % ticketsTotal;			// Consigo el ticket ganador
+		contador = 0; 							// Reseteo contador para el proximo
+		int indiceGanador = sorteo(ticketGanador);
+		sacarTicketsDeMas(indiceGanador);
+		return q[indiceGanador].pid;
+	}else if (quantum != contador){ 					// Todavia no terminé sigo, en caso de que haya terminado el tiempo pero no la tarea y sea la 												última entra en el caso anterior y el sorteo lo gana el que está solo;
+		return current_pid();
+	}else{									// No tengo ni tareas 
+		return IDLE_TASK;
+	}
+}
+
+int SchedLottery::haceExit(){
+	int i = buscarProcesoEnCola(current_pid());
+	ticketsTotal -= q[i].tickets;
+	q.erase(q.begin() + i);
+	if (ticketsTotal > 0) { //Quedan tareas en la cola de activas
+		int ticketGanador = random() % ticketsTotal;			// Consigo el ticket ganador
+		contador = 0; 							// Reseteo contador para el proximo
+		int indiceGanador = sorteo(ticketGanador);
+		sacarTicketsDeMas(indiceGanador);
+		return q[indiceGanador].pid;
+	}else{
+		return IDLE_TASK;	
+	}
+}
+
+int SchedLottery::buscarProcesoEnCola(int pid) {
+	int i = 0;
+	while(true)
+	{
+		if(q[i].pid == pid)
+		{
+			return i;						// Asumimos que el proceso que busco esta en la cola por precondicion
+		}
+		i++;
+	}
+}
+
+int SchedLottery::sorteo(int ticketGanador){
+		int sumaTickets = 0;
+		int i = 0;
+		while(true)
+		{
+			if(q[i].estado != BLOQUEADO)				
+			{
+				sumaTickets += q[i].tickets;			// Sumo los tickets hasta llegar al que salió
+				if(sumaTickets > ticketGanador) break;		// Es mayor, porque si sale cero se le asigna al primero y si sale n-1 se le asigna al ultimo
+			}							// Si fuera mayor o igual, estariamos asignando 2 al primero y cero al ultimo
+			i++;
+		}
+		return i;
+}
+
+void SchedLottery::sacarTicketsDeMas(int indiceGanador){
+	if (q[indiceGanador].estado == SOBRAN_TICKETS){			// Si fue compensado hay que sacarle los que tiene de más
+			ticketsTotal = ticketsTotal - q[indiceGanador].tickets - 1;				
+			q[indiceGanador].tickets = 1;
+	}
 }
--- tp1/simusched/sched_lottery.h
+++ tp1/simusched/sched_lottery.h
@@ -2,8 +2,17 @@
 #define __SCHED_LOTTERY__
 
 #include <vector>
+#include <algorithm>
 #include <queue>
 #include "basesched.h"
+
+#define NORMAL -1
+#define BLOQUEADO 0
+#define SOBRAN_TICKETS 1
+
+//NORMAL = no esta bloqueado y no debe nada, esto es el estado despues de que salio sorteado o que nunca se bloqueó o quue ya devolvió
+//BLOQUEADO = esta bloqueado y tiene tickets de mas, este es el estado de bloqueado
+//TICKETS_SOBRAN = no esta bloqueado pero tiene tickets de mas que solo sirven para los n rounds hasta que sale sorteado, este es el estado cunado desbloquea
 
 class SchedLottery : public SchedBase {
 	public:
@@ -11,8 +20,30 @@ class SchedLottery : public SchedBase {
 		virtual void load(int pid);
 		virtual void unblock(int pid);
 		virtual int tick(const enum Motivo m);
+		int haceTick();
+		int haceBlock();
+		int haceExit();
+		int buscarProcesoEnCola(int pid);
+		int sorteo(int ticketGanador);
+		void sacarTicketsDeMas(int pidGanador);
 	
 	private:
+		
+		struct tarea
+		{
+			tarea() {};
+			tarea(int pid, int tickets, int estado) : pid(pid), tickets(tickets), estado(estado) {};
+	
+			int pid;
+			int tickets;
+			int estado;
+		};
+
+
+		std::vector<tarea> q;
+		int ticketsTotal;
+		int quantum;
+		int contador;
 };
 
 #endif
