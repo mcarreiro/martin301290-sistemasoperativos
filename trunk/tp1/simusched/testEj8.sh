85f2dd29ba7b309b0e413ff204155ec3d6ac713c
martin301290@gmail.com <martin301290@gmail.com@bc70af33-96d5-9d1e-8836-0a54cdf17148>
2012-06-13 00:02:54 +0000

    ej8 con lo nuevo

--- trunk/tp1/simusched/testEj8.sh
+++ trunk/tp1/simusched/testEj8.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 rm -rf outputs/ej8/*
 ./simusched lotes/loteEj8.tsk 1 SchedLottery 1 3 >> outputs/ej8/sl-ej8-1-3
-#./simusched lotes/loteEj8.tsk 1 SchedLottery 2 6 >> outputs/ej8/sl-ej8-2-6
-./simusched lotes/loteEj8.tsk 1 SchedLottery 5 2 >> outputs/ej8/sl-ej8-5-2
-./simusched lotes/loteEj8.tsk 1 SchedLottery 3 5 >> outputs/ej8/sl-ej8-3-5
-./simusched lotes/loteEj8.tsk 1 SchedLottery 4 7 >> outputs/ej8/sl-ej8-4-7
-#./simusched lotes/loteEj8.tsk 1 SchedLottery 1 8 >> outputs/ej8/sl-ej8-1-8
-./simusched lotes/loteEj8.tsk 1 SchedLottery 2 12 >> outputs/ej8/sl-ej8-2-12
+./simusched lotes/loteEj8.tsk 1 SchedLottery 5 3 >> outputs/ej8/sl-ej8-5-2
+./simusched lotes/loteEj8.tsk 1 SchedLottery 3 3 >> outputs/ej8/sl-ej8-3-5
+./simusched lotes/loteEj8.tsk 1 SchedLottery 4 3 >> outputs/ej8/sl-ej8-4-7
+#IO
+#./simusched lotes/loteEj8IO.tsk 1 SchedLottery 1 8 >> outputs/ej8/sl-ej8-1-8
+./simusched lotes/loteEj8.tsk 1 SchedLottery 2 3 >> outputs/ej8/sl-ej8-2-12
 ./graphsched.py outputs/ej8/sl-ej8-1-3
-#./graphsched.py outputs/ej8/sl-ej8-2-6
 ./graphsched.py outputs/ej8/sl-ej8-5-2
 ./graphsched.py outputs/ej8/sl-ej8-3-5
 ./graphsched.py outputs/ej8/sl-ej8-4-7
+#IO
 #./graphsched.py outputs/ej8/sl-ej8-1-8
 ./graphsched.py outputs/ej8/sl-ej8-2-12
