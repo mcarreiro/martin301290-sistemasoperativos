dade2d8add2ee3d7a1d174c25226b91abdad4e93
martin301290@gmail.com <martin301290@gmail.com@bc70af33-96d5-9d1e-8836-0a54cdf17148>
2012-06-20 00:56:25 +0000

    srv

--- tp3/src/srv.h
+++ tp3/src/srv.h
@@ -2,6 +2,9 @@
 #define __srv_h__
 
 #include "tp3.h"
+#define TAG_REQUEST           60    
+#define TAG_REPLY             70    
+#define TAG_SERVER_DOWN        80    
 
 void servidor(int mi_cliente);
 
