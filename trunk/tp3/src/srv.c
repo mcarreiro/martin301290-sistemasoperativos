a9b2cb2ef70f0fb991e8d76b6ca61cfd82d0485e
martin301290@gmail.com <martin301290@gmail.com@bc70af33-96d5-9d1e-8836-0a54cdf17148>
2012-06-20 00:56:25 +0000

    srv

--- trunk/tp3/src/srv.c
+++ trunk/tp3/src/srv.c
@@ -8,39 +8,90 @@
 
 void servidor(int mi_cliente)
 {
-    MPI_Status status; int origen, tag;
-    int hay_pedido_local = FALSE;
-    int listo_para_salir = FALSE;
-    
-    while( ! listo_para_salir ) {
-        
-        MPI_Recv(NULL, 0, MPI_INT, ANY_SOURCE, ANY_TAG, COMM_WORLD, &status);
-        origen = status.MPI_SOURCE;
-        tag = status.MPI_TAG;
-        
-        if (tag == TAG_PEDIDO) {
-            assert(origen == mi_cliente);
-            debug("Mi cliente solicita acceso exclusivo");
-            assert(hay_pedido_local == FALSE);
-            hay_pedido_local = TRUE;
-            debug("Dándole permiso (frutesco por ahora)");
-            MPI_Send(NULL, 0, MPI_INT, mi_cliente, TAG_OTORGADO, COMM_WORLD);
-        }
-        
-        else if (tag == TAG_LIBERO) {
-            assert(origen == mi_cliente);
-            debug("Mi cliente libera su acceso exclusivo");
-            assert(hay_pedido_local == TRUE);
-            hay_pedido_local = FALSE;
-        }
-        
-        else if (tag == TAG_TERMINE) {
-            assert(origen == mi_cliente);
-            debug("Mi cliente avisa que terminó");
-            listo_para_salir = TRUE;
-        }
-        
-    }
-    
+	int clock = 0;
+	int cantRanks = cant_ranks;
+	int cantReply = 0;
+	MPI_Status status; int origen, tag, tagServidor, origenServidor;
+	int hay_pedido_local = FALSE;
+	int listo_para_salir = FALSE;
+	int buffer, count = 1;
+	int replyPendientes[cantRanks/2];
+	int i = 0;
+	while(i<cantRanks/2){
+		replyPendientes[i] = FALSE;	
+		i++;
+	}
+	while( ! listo_para_salir ) {
+
+		MPI_Recv(&buffer, count, MPI_INT, ANY_SOURCE, ANY_TAG, COMM_WORLD, &status);
+		origen = status.MPI_SOURCE;
+		tag = status.MPI_TAG;
+		clock++;
+		if (tag == TAG_PEDIDO) {
+			assert(origen == mi_cliente);
+			debug("Mi cliente solicita acceso exclusivo");
+			assert(hay_pedido_local == FALSE);
+			hay_pedido_local = TRUE;
+			i = 0;
+			while(i<cantRanks){
+				clock++;
+				if(i!=mi_cliente-1) //No me lo mando a mi mismo
+					MPI_Send(&clock, 1, MPI_INT, i, TAG_REQUEST, COMM_WORLD);				
+				i+=2;
+			}
+			while(cantReply != (cantRanks-1)/2){
+				MPI_Recv(&buffer, count, MPI_INT, ANY_SOURCE, ANY_TAG, COMM_WORLD, &status);	
+				tagServidor = status.MPI_TAG;
+				origenServidor = status.MPI_SOURCE;
+				switch(tagServidor){
+					case TAG_REQUEST:
+							debug("ME LLEGO REQUEST DE" + origenServidor);
+							if(buffer<clock){
+								MPI_Send(&clock, 1, MPI_INT, origenServidor, TAG_REPLY, COMM_WORLD);											
+							} else {
+								clock = buffer + 1;	
+								replyPendientes[origenServidor/2] = TRUE;											
+							}
+							break;
+					case TAG_REPLY: debug("ME LLEGO REPLY DE" + origenServidor); cantReply++;
+							break;
+					case TAG_SERVER_DOWN:
+							//MATAR A TODOS 
+							break;			
+				}
+
+			}
+			debug("Dándole permiso (frutesco por ahora) => COPADO");
+			MPI_Send(NULL, 0, MPI_INT, mi_cliente, TAG_OTORGADO, COMM_WORLD);
+		}
+
+		else if (tag == TAG_LIBERO) {
+			assert(origen == mi_cliente);
+			debug("Mi cliente libera su acceso exclusivo");
+			assert(hay_pedido_local == TRUE);
+			hay_pedido_local = FALSE;
+			i = 0;
+			while(i<cantRanks/2){
+				if(replyPendientes[i]==TRUE){
+					debug("REPLY PENDIENTE A" + i*2);
+					MPI_Send(&clock, 1, MPI_INT, i*2, TAG_REPLY, COMM_WORLD);																
+				}	
+				i++;
+			}
+		}
+
+		else if (tag == TAG_TERMINE) {
+			assert(origen == mi_cliente);
+			debug("Mi cliente avisa que terminó");
+			listo_para_salir = TRUE;
+		}
+		else if(tag == TAG_REQUEST){
+			debug("ME LLEGO UN PUTO REQUEST SOLO");
+			clock++;
+			MPI_Send(&clock, 1, MPI_INT, origen, TAG_REPLY, COMM_WORLD);												
+		}
+
+	}
+
 }
 
