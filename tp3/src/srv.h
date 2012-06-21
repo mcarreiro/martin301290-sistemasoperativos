77a5ed14488cde3e2aa446d1e4ba58f8fe1030b1
jmanuoz@gmail.com <jmanuoz@gmail.com@bc70af33-96d5-9d1e-8836-0a54cdf17148>
2012-06-21 21:30:20 +0000

    pedido

--- tp3/src/srv.h
+++ tp3/src/srv.h
@@ -4,8 +4,10 @@
 #include "tp3.h"
 #define TAG_REQUEST           60    
 #define TAG_REPLY             70    
-#define TAG_SERVER_DOWN        80    
+#define TAG_SERVER_DOWN       80    
+#define TAG_GOAWAY		      90  
 
 void servidor(int mi_cliente);
 
+void handle(int tag, int pendingResponses[], int statusServers[], int origen, int mi_cliente,MPI_Status status);
 #endif
