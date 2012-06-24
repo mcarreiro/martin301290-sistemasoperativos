577160d0f3da61eb7da7361fe9d371761d953863
martin301290@gmail.com <martin301290@gmail.com@bc70af33-96d5-9d1e-8836-0a54cdf17148>
2012-06-24 14:27:41 +0000

    con comentarios

--- tp3/src/srv.c
+++ tp3/src/srv.c
@@ -27,29 +27,31 @@ void servidor(int mi_cliente)
 	int Highest_Sequence_Number = 0;
 	char str [100];
 	int cantRespuestas = 0;
-	
+	//Al empezar nadie pidió nada, nadie se quiere ir y están todos arriba
 	while(i<cantRanks/2){
 		replyPendientes[i] = FALSE;	
 		goAwayPendientes[i] = FALSE;
+		estadoServidores[i] = TRUE;
 		i++;
 	}
-	for(i = 0; i < cant_ranks / 2; i++) estadoServidores[i] = TRUE;
 	
-	while( ! listo_para_salir ) {
+	while( ! listo_para_salir ) { //Hasta que no me quiera ir y me dejen ir, sigo en juego
 
 		MPI_Recv(&buffer, count, MPI_INT, ANY_SOURCE, ANY_TAG, COMM_WORLD, &status);
 		origen = status.MPI_SOURCE;
 		tag = status.MPI_TAG;
-		
+		//Recibí un mensaje, hay que ver qué era...
 		if (tag == TAG_PEDIDO) {
+			//Pedido de sección crítica de mi cliente
 			assert(origen == mi_cliente);
 			debug("Mi cliente solicita acceso exclusivo");
 			assert(hay_pedido_local == FALSE);
 			hay_pedido_local = TRUE;
 			i = 0;
+			//Al ser un pedido seteo mi clock con el último que guardé del último request que recibí
 			clock = Highest_Sequence_Number + 1;
 			while(i<cantRanks){
-				
+				//Mando el pedido al resto de los servidores activos
 				if(i!=mi_cliente-1 && estadoServidores[i/2] == TRUE) //No me lo mando a mi mismo y se lo mande alguien que este vivo
 					MPI_Send(&clock, 1, MPI_INT, i, TAG_REQUEST, COMM_WORLD);				
 				i+=2;
@@ -58,15 +60,21 @@ void servidor(int mi_cliente)
 			cantReply = 0;	
 			cantRespuestas = (servidores - 1);
 			while(cantReply < cantRespuestas){
+				//Espero las respuestas antes de darle el OK a mi cliente
 				MPI_Recv(&buffer, count, MPI_INT, ANY_SOURCE, ANY_TAG, COMM_WORLD, &status);	
 				tagServidor = status.MPI_TAG;
 				origenServidor = status.MPI_SOURCE;
+				//Mientras espero las respuestas veo qué tipo de mensajes recibo
 				switch(tagServidor){
 					case TAG_REQUEST:
+							//Recibí otro request, hay que ver la prioridad basado en el clock de cada uno y en caso de ser iguales el número de rank
 							debug("ME LLEGO REQUEST DE" + origenServidor);
 							if(buffer<clock){
+								//Tiene menor clock, entonces tiene prioridad
 								MPI_Send(&clock, 1, MPI_INT, origenServidor, TAG_REPLY, COMM_WORLD);											
-							} else if(buffer>clock) {								
+							} else if(buffer>clock) {				
+								//Tengo más prioridad, pero entonces encontré hasta ahora el clock más alto, 	
+								//entonces mi proximo pedido debe tener uno más que este			
 								Highest_Sequence_Number = buffer;
 								replyPendientes[origenServidor/2] = TRUE;											
 							}else{
@@ -74,9 +82,9 @@ void servidor(int mi_cliente)
 										else replyPendientes[origenServidor/2] = TRUE;
 							}
 							break;
-					case TAG_REPLY: debug("ME LLEGO REPLY DE" + origenServidor); cantReply++;
+					case TAG_REPLY: debug("ME LLEGO REPLY DE" + origenServidor); cantReply++; //Cuento nuevo reply
 							break;
-					case TAG_SERVER_DOWN:
+					case TAG_SERVER_DOWN: //Si se quiere ir, resto la cantidad de servidores pero no sumo cantReply, porque estaría avanzando en dos los OK
 							servidores--;											
 							estadoServidores[origenServidor/2] = FALSE;
 							MPI_Send(NULL, 0, MPI_INT, origenServidor, TAG_GOAWAY, COMM_WORLD);
@@ -86,10 +94,12 @@ void servidor(int mi_cliente)
 
 			}
 			debug("Dándole permiso (frutesco por ahora) => COPADO");
+			//Recibí todos los reply necesarios, entonces mi cliente tiene exclusión mutua sobre el recurso
 			MPI_Send(NULL, 0, MPI_INT, mi_cliente, TAG_OTORGADO, COMM_WORLD);
 		}
 
 		else if (tag == TAG_LIBERO) {
+			//Mi cliente terminó debo liberar y darle el REPLY a los que antes retrasé
 			assert(origen == mi_cliente);
 			debug("Mi cliente libera su acceso exclusivo");
 			assert(hay_pedido_local == TRUE);
@@ -106,6 +116,7 @@ void servidor(int mi_cliente)
 		}
 
 		else if (tag == TAG_TERMINE) {
+			//Como mi cliente terminó, entonces no tengo que hacer nada más como servidor y me quiero ir
 			assert(origen == mi_cliente);
 				debug("Mi cliente avisa que terminó");
 				listo_para_salir = TRUE;
@@ -113,7 +124,6 @@ void servidor(int mi_cliente)
 				
 				//Les digo a mis amigos que me quiero ir
 				i = 0;
-
 				while(i<cantRanks){					
 					if(i!=mi_cliente-1 && estadoServidores[i/2] == TRUE) 
 						MPI_Send(&clock, 1, MPI_INT, i, TAG_SERVER_DOWN, COMM_WORLD);				
@@ -172,7 +182,7 @@ void servidor(int mi_cliente)
 					}
 				}
 				debug("ya les avise");
-				//ya me puedo ir, pero como soy buena onda les aviso a los demas que tambien pueden irse
+				//ya me puedo ir, pero les aviso a los demas que tambien pueden irse
 				for(i = 0; i < cant_ranks / 2; i++)
 				{
 					if(goAwayPendientes[i] == TRUE && estadoServidores[i] == TRUE)
@@ -185,6 +195,7 @@ void servidor(int mi_cliente)
 				}	
 		}
 		else if(tag == TAG_REQUEST){
+			//Me llegó un pedido y no estaba haciendo nada entoces veo si mi cliente sigue laburando
 			if(!hay_pedido_local){
 				debug("LLEGO UN REQUEST QUE ACEPTO Y CONTESTO CON REPLY");
 				
@@ -193,10 +204,11 @@ void servidor(int mi_cliente)
 				debug("MI CLIENTE NO LIBERO TODAVIA, WAIT");
 				replyPendientes[origen/2] = TRUE;	
 			}
-			
+			//Al igual que antes encontré un clock más alto, entonces lo guardo para el próximo request
 			if(Highest_Sequence_Number < buffer) Highest_Sequence_Number = buffer;								
 		}
 		else if(tag == TAG_SERVER_DOWN){
+			//Alguien se quiere ir
 			servidores--;
 			estadoServidores[origen/2] = FALSE;
 			MPI_Send(NULL, 0, MPI_INT, origen, TAG_GOAWAY, COMM_WORLD);						
